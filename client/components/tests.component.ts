8e6e875f40e25a93c4808ab198b88afa228ab463
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2016-08-25 09:11:24 +0200

    Max no of words is now taken from settings

--- client/components/tests.component.ts
+++ client/components/tests.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Filter} from './filter.component';
 import {WordPair} from '../models/word.model';
 import {Filter as FilterModel} from '../models/filters.model';
 import {WordService} from '../services/words.service';
 import {RestartService} from '../services/restart.service';
 import {ErrorService} from '../services/error.service';
+import {SettingsService} from '../services/settings.service';
 import {Subscription}   from 'rxjs/Subscription';
 import {shuffle} from '../utils/utils';
 
@@ -67,7 +68,7 @@ import {shuffle} from '../utils/utils';
   `li {cursor:pointer;}`]
 })
 
-export class Tests implements OnDestroy {
+export class Tests implements OnInit, OnDestroy {
   maxWords = 20;
   listType: string = 'filter';
   started: boolean = false;
@@ -78,6 +79,7 @@ export class Tests implements OnDestroy {
   constructor(
     private wordService: WordService,
     private errorService: ErrorService,
+    private settingsService: SettingsService,
     restartService: RestartService) {
     this.subscription = restartService.restartFilter$.subscribe(
       start => {
@@ -85,46 +87,49 @@ export class Tests implements OnDestroy {
       });
   }
 
+  ngOnInit() {
+    this.settingsService.getAppSettings().then(
+      settings => {this.maxWords = settings.all.maxWords;},
+      error => this.errorService.handleError(error)
+    );
+  }
+
   selectListType(tpe: string) {
     this.listType = tpe;
   }
 
   getWordsFromFilter(filter: FilterModel) {
-    this.wordService.getWordsFromFilter(filter, this.maxWords)
-      .then(
-        words => {
-          this.cards = words;
-          this.exerciseTpe = filter.test;
-          this.started = true;
-        },
-        error => this.errorService.handleError(error)
-      );
+    this.wordService.getWordsFromFilter(filter, this.maxWords).then(
+      words => {
+        this.cards = words;
+        this.exerciseTpe = filter.test;
+        this.started = true;
+      },
+      error => this.errorService.handleError(error)
+    );
   }
 
   getWordsFromUserList(data: any) {
-    this.wordService.getWordsFromWordList(data.selected._id, this.maxWords)
-      .then(
-        words => {
-          this.cards = words;
-          this.exerciseTpe = data.exerciseTpe;
-          this.started = true;
-        },
-        error => this.errorService.handleError(error)
-      );
+    this.wordService.getWordsFromWordList(data.selected._id, this.maxWords).then(
+      words => {
+        this.cards = words;
+        this.exerciseTpe = data.exerciseTpe;
+        this.started = true;
+      },
+      error => this.errorService.handleError(error)
+    );
   }
 
   getWordsFromAutoList(data: any) {
-    this.wordService.getWordsFromAutoList(data.selected.id, this.maxWords)
-      .then(
-        words => {
-          this.cards = words;
-          this.exerciseTpe = data.exerciseTpe;
-          this.started = true;
-        },
-        error => this.errorService.handleError(error)
-      );
+    this.wordService.getWordsFromAutoList(data.selected.id, this.maxWords).then(
+      words => {
+        this.cards = words;
+        this.exerciseTpe = data.exerciseTpe;
+        this.started = true;
+      },
+      error => this.errorService.handleError(error)
+    );
   }
-
 
   restart() {
     shuffle(this.cards);
