39bb8d9d44f4e809294810468d1066816ec3d180
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2017-02-06 20:27:29 +0100

    Added progress calendar

--- client/components/progress.component.ts
+++ client/components/progress.component.ts
@@ -8,23 +8,42 @@ import {ProgressStats} from '../models/stats.model';
 import 'rxjs/add/operator/takeWhile';
 import * as moment from 'moment';
 
+interface CalendarDay {
+  day: string;
+  currentMonth: boolean;
+}
+
 @Component({
   template: `
     <h1>Progress</h1>
-    <div class="row month">
+    <div class="row days">
       <div *ngFor="let col of cols;" class="col-md-1">
-        {{days[col]}}
+        {{dayNames[col]}}
       </div>
       <div class="col-md-5">extra</div>
     </div>
-  `
+    <div class="row" *ngFor="let row of rows">
+      <div *ngFor="let col of cols;" class="col-md-1">
+        <span [ngClass]="{'diffMonth': !calendarDays[row * 7 + col].currentMonth}">{{calendarDays[row * 7 + col].day}}</span>
+      </div>
+      <div class="col-md-5">extra</div>
+    </div>
+  `,
+  styles: [`
+    .diffMonth {
+      color: #cccccc;
+      font-style: italic;
+    }
+  `]
 })
 
 export class Progress implements OnInit, OnDestroy {
   componentActive: boolean = true;
   stats: ProgressStats;
   cols: number[];
-  days: string[];
+  rows: number[];
+  dayNames: string[];
+  calendarDays: CalendarDay[] = [];
 
   constructor (
     private authService: AuthService,
@@ -45,13 +64,25 @@ export class Progress implements OnInit, OnDestroy {
 
   buildCalendar() {
     this.cols = new Array(7).fill(1).map((x, i) => i);
-    this.days = this.utilsService.getDaysNames('short');
+    this.rows = new Array(5).fill(1).map((x, i) => i);
+    this.dayNames = this.utilsService.getDaysNames('short');
 
-    let date = new Date(),
+    //get  first day of the month
+    const date = new Date(),
         y = date.getFullYear(),
         m = date.getMonth();
-    let firstDay = new Date(y, m, 1);
-    console.log('first day of month', firstDay);
+    const firstDay = new Date(y, m, 1);
+    //get first monday of that week
+    const firstMonday = moment(firstDay).startOf('isoWeek');
+    let calendarDay = moment(firstMonday);
+    // get all days for the calendar
+    for (let indx = 0; indx < 35; indx++) {
+      this.calendarDays[indx] = {
+        day: calendarDay.format('DD'),
+        currentMonth: calendarDay.month() === moment(firstDay).month()
+      };
+      calendarDay = calendarDay.add(1, 'd');
+    }
   }
 
   fetchStats() {
@@ -61,7 +92,6 @@ export class Progress implements OnInit, OnDestroy {
     .takeWhile(() => this.componentActive)
     .subscribe(
       stats => {
-        console.log('fetched stats', stats);
         this.stats = stats;
         statsFetched = true;
       },
