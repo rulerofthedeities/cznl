735199048612f3c899c7ed8a75ae1637adb9832a
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2017-02-26 14:51:45 +0100

    Replaced data folder with service method

--- src/app/services/filters.service.ts
+++ src/app/services/filters.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
+import {UtilsService} from './utils.service';
 import {Http, Headers} from '@angular/http';
-import {LEVELS, TPES, GENUS, CASES} from '../data/filters';
 import {Observable} from 'rxjs/Observable';
 import {Cat} from '../models/filters.model';
 
@@ -10,31 +10,35 @@ export class FilterService {
 
   constructor(
     private authService: AuthService,
+    private utilsService: UtilsService,
     private http: Http
   ) {}
 
   getFilterOptions() {
-    const token = this.authService.getToken();
-    let headers = new Headers();
+    const token = this.authService.getToken(),
+          headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Authorization', 'Bearer ' + token);
     return this.http
       .get('/api/cats?search=&max=250', {headers})
       .map(response => {
-        let cats = response.status === 200 ? response.json().cats: [];
+        const filterCats = response.status === 200 ? response.json().cats : [];
         return {
-        'levels': LEVELS,
-        'tpes': TPES,
-        'genus': GENUS,
-        'cases': CASES,
-        'cats': this.catsToPropercase(cats)};
+        'levels': this.utilsService.getFilter('levels'),
+        'tpes': this.utilsService.getFilter('tpes'),
+        'genus': this.utilsService.getFilter('genus'),
+        'cases': this.utilsService.getFilter('cases'),
+        'cats': this.catsToPropercase(filterCats)};
       })
       .catch(error => Observable.throw(error));
   }
 
   private catsToPropercase(arr: Cat[]) {
     return arr.map(cat => {
-      return {'name': cat.name[0].toUpperCase() + cat.name.substring(1), 'total': cat.total};
+      return {
+        'name': cat.name[0].toUpperCase() + cat.name.substring(1),
+        'total': cat.total
+      };
     });
   }
 
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,3 +1,5 @@
+import {FilterOption} from '../models/filters.model';
+
 export class UtilsService {
   // https://basarat.gitbooks.io/algorithms/content/docs/shuffling.html
   shuffle<T>(array: T[]): T[] {
@@ -16,7 +18,7 @@ export class UtilsService {
     return array;
   }
 
-  getDaysNames(tpe: string = 'long') {
+  getDaysNames(tpe: string = 'long'): string[] {
     const days = {
       long: [
         'maandag',
@@ -39,6 +41,56 @@ export class UtilsService {
     };
 
     return days[tpe];
+  }
+
+  getFilter(tpe: string): FilterOption[] {
+    let filter: FilterOption[] = [];
+    switch (tpe) {
+      case 'levels':
+        filter = [
+          {label: 'Elementair', val: 0},
+          {label: 'Basis', val: 1},
+          {label: 'Medium', val: 2},
+          {label: 'Hoog', val: 3}
+        ];
+        break;
+      case 'tpes':
+        filter = [
+          {label: 'Zelfst. naamwoord', val: 'noun'},
+          {label: 'Bijv. naamwoord', val: 'adj'},
+          {label: 'Bijwoord', val: 'adv'},
+          {label: 'Werkwoord', val: 'verb'},
+          {label: 'Voegwoord', val: 'conj'},
+          {label: 'Voorzetsel', val: 'prep'},
+          {label: 'Tussenwerpsel', val: 'interj'},
+          {label: 'Voornaamwoord', val: 'pronoun'},
+          {label: 'Eigennaam', val: 'propernoun'},
+          {label: 'Telwoord', val: 'numeral'},
+          {label: 'Partikel', val: 'part'},
+          {label: 'Uitdrukking', val: 'phrase'}
+        ];
+        break;
+      case 'genus':
+        filter = [
+          {label: 'Ma', val: 'Ma'},
+          {label: 'Mi', val: 'Mi'},
+          {label: 'F', val: 'F'},
+          {label: 'N', val: 'N'}
+        ];
+        break;
+      case 'cases':
+        filter = [
+          {label: 'N', val: '1'},
+          {label: 'G', val: '2'},
+          {label: 'D', val: '3'},
+          {label: 'A', val: '4'},
+          {label: 'V', val: '5'},
+          {label: 'L', val: '6'},
+          {label: 'I', val: '7'},
+        ];
+        break;
+    }
+    return filter;
   }
 
   private getRandom(floor: number, ceiling: number) {
